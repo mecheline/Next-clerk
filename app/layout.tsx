6203c6993b8ff251ff14d43a46640be6fb5d349f
mecheline <ucheamaechi2016@gmail.com>
2025-01-22 15:48:28 +0100

    fix/vercel errrors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+
 import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
